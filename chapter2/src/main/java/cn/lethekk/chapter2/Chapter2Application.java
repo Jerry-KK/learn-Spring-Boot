ed69a6193845955d348091fe21d34e4f75e3c283
lethekk <1312226181@qq.com>
2020-08-11 12:13:32 +0800

    第二章 开发环境搭建和基本开发

---
+++ chapter2/src/main/java/cn/lethekk/chapter2/Chapter2Application.java
@@ -0,0 +1,13 @@
+package cn.lethekk.chapter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter2Application.class, args);
+    }
+
+}
---
+++ chapter2/src/test/java/cn/lethekk/chapter2/Chapter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lethekk.chapter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
